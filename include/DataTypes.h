1f67259ffc74932d8eb2ffa4fd2f063ae2ef262d
MakoEnergy <MakoEnergy@users.noreply.github.com>
2018-10-14 17:59:16 -0500

    Suppressing additional VC warnings.

--- include/DataTypes.h
+++ include/DataTypes.h
@@ -37,8 +37,8 @@
    Joseph Toppi - toppij@gmail.com
    John Blackwood - makoenergy02@gmail.com
 */
-#ifndef Mezz_StaticFoundation_datatypes_h
-#define Mezz_StaticFoundation_datatypes_h
+#ifndef Mezz_StaticFoundation_DataTypes_h
+#define Mezz_StaticFoundation_DataTypes_h
 
 ///////////////////////////////////////////////////////////////////////////////
 // Any Special data types that we need will get declared right here
@@ -58,6 +58,7 @@
 
     SAVE_WARNING_STATE
     SUPPRESS_VC_WARNING(4061)
+    SUPPRESS_VC_WARNING(4548)
 
     #include <stdint.h>
     #include <cstddef>
