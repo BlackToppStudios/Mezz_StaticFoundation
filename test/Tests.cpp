18ab12b59d508b5d65ccc571c2be78d718a57697
MakoEnergy <MakoEnergy@users.noreply.github.com>
2018-10-14 20:02:30 -0500

    Wrapped more standard includes in warning suppression.

--- test/Tests.cpp
+++ test/Tests.cpp
@@ -38,10 +38,16 @@
    John Blackwood - makoenergy02@gmail.com
 */
 
+SAVE_WARNING_STATE
+SUPPRESS_VC_WARNING(4548) // This was added to suppress a warning in MSVC's implementation 
+                          // of malloc.h where they use a comma in an assert.
+
 #include <cstdlib>
 #include <iostream>
 #include <sstream>
 #include <fstream>
+
+RESTORE_WARNING_STATE
 
 #include "Tests.h"
 #include "RuntimeStatics.h"
